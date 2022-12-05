936d742b1097745e4c21a1a3a3f2016b567ea022
App Academy Student <email@email.com>
2022-12-05 15:05:06 -0500

    hashmap

--- skeleton/lib/p03_hash_set.rb
+++ skeleton/lib/p03_hash_set.rb
@@ -1,18 +1,38 @@
-class HashSet
-  attr_reader :count
+class HashSet 
+   attr_reader :count
 
-  def initialize(num_buckets = 8)
+  def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
   end
 
-  def insert(key)
+  def insert(num)
+
+    resize! if num_buckets == count
+    unless self.include?(num)
+      @count += 1
+      @store[num.hash % num_buckets] << num
+    end
+
   end
 
-  def include?(key)
+  def count
+    @count
   end
 
-  def remove(key)
+  def remove(num)
+
+    if self.include?(num)
+      @store[num.hash % num_buckets].delete(num)
+      @count -=1
+    end
+
+  end
+
+  def include?(num)
+
+    @store[num.hash % @store.length].include?(num)
+
   end
 
   private
@@ -26,5 +46,18 @@ class HashSet
   end
 
   def resize!
+    
+    new_arr = Array.new(num_buckets*2) {Array.new}
+
+    @store.each {|el| 
+        
+          
+      el.each {|num| 
+      new_arr[num.hash % new_arr.length] << num if !num.nil? }
+        
+    }
+        
+    @store = new_arr
+      
   end
 end
